3be0f839ddf12a243e8fc20dedb9499d9724b9b7
Dionatan Voss <42809143+dionevoss@users.noreply.github.com>
2026-02-27 00:49:08 -0300

    feat: add llmchain legacy pitch (#1)

--- src/llmchain/llmchain.ts
+++ src/llmchain/llmchain.ts
@@ -1,15 +1,49 @@
 import { PromptTemplate } from "@langchain/core/prompts";
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import { StringOutputParser } from "@langchain/core/output_parsers";
+import dotenv from "dotenv";
+import { LLMChain } from "@langchain/classic/chains";
 
-const prompt = new PromptTemplate({
-  inputVariables: ["course", "role", "wordLimit"],
-  template:
-    "Describe the importance of learning {course} for {role}. Limit the output to {wordLimit} words.",
-});
+dotenv.config();
 
-const formattedPrompt = await prompt.format({
-  course: "generative AI",
-  role: "javascript developers",
-  wordLimit: "50",
-});
+async function personalizedPitch(
+  course: string,
+  role: string,
+  wordLimit: string,
+) {
+  const prompt = new PromptTemplate({
+    inputVariables: ["course", "role", "wordLimit"],
+    template:
+      "Describe the importance of learning {course} for {role}. Limit the output to {wordLimit} words.",
+  });
 
-console.log("Formatted Prompt: ", formattedPrompt);
+  const formattedPrompt = await prompt.format({
+    course,
+    role,
+    wordLimit,
+  });
+
+  console.log("Formatted Prompt: ", formattedPrompt);
+
+  const llm = new ChatGoogleGenerativeAI({
+    model: "gemini-2.5-flash",
+  });
+
+  const outputParser = new StringOutputParser();
+
+  const legacyLLM = new LLMChain({
+    llm,
+    prompt,
+    outputParser,
+  });
+
+  const result = await legacyLLM.invoke({
+    course,
+    role,
+    wordLimit,
+  });
+
+  console.log("Generated Output: ", result);
+}
+
+await personalizedPitch("generative AI", "javascript developers", "50");
